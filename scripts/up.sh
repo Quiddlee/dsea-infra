4a71f81f032f135c85a4af9c25066de5b683f6e0
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-12-15 16:23:08 +0200

    chore: update docker setup

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd "$ROOT_DIR"
+
 ./scripts/bootstrap.sh
-docker compose -f compose/docker-compose.yml up --build
+
+docker compose \
+  --env-file "$ROOT_DIR/.env" \
+  -f compose/docker-compose.yml \
+  up --build
